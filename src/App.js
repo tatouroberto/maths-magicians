b7282467d51f87d7c6bfd5863d8a2f1427c390b5
tatouroberto <tatouroberto@gmail.com>
2023-10-22 01:38:21 +0200

    add the ninja component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/ninja';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
